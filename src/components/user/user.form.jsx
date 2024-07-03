a8a0ccee30aa012160de6c834a51faad7e9fbee9
AoTuTap <thinhnpse98@gmail.com>
2024-07-04 00:54:16 +0200

    Create axios to call API

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,5 @@
 import { Button, Input } from "antd"
+import axios from "axios";
 import { useState } from "react"
 
 const UserForm = () => {
@@ -8,7 +9,14 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     const handleClickButton = () => {
-        alert("Click me")
+        const URL_BACKEND = "http://localhost:8080/api/v1/user";
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phone: phone
+        }
+        axios.post(URL_BACKEND, data)
     };
 
     return (
